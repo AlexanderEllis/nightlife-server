e4fb28e958c1d00b5a5d6ee0173eb5eff816b26e
Alexander Ellis <ellisonjtk@gmail.com>
2017-05-02 21:14:11 -0400

    redoing directory organization of course

---
+++ app.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var app = express();
+
+let bars = [{
+  type: 'bars',
+  id: 'sports-bar',
+  attributes: {
+    name: 'Sports Bar',
+    location: 'Brighton, MA',
+    going: 0,
+    rating: 4,
+    description: 'This is a great bar to catch a game at.',
+    image: 'http://www.hermarys.com/wp-content/uploads/sports-bar-600x360.png'
+  }
+}, {
+  type: 'bars',
+  id: 'dive-bar',
+  attributes: {
+    name: 'Dive Bar',
+    location: 'Allston, MA',
+    going: 0,
+    rating: 3,
+    description: 'This is a great bar to have a cheap beer.',
+    image: 'https://divebardiscourse.files.wordpress.com/2015/09/dive-counter.jpg'
+  }
+}, {
+  type: 'bars',
+  id: 'wine-bar',
+  attributes: {
+    name: 'Wine Bar',
+    location: 'Brookline, MA',
+    going: 0,
+    rating: 5,
+    description: 'This is a great bar to have wine at.',
+    image: 'https://vinepair.com/wp-content/uploads/2015/10/Krog-Bar.jpg'
+  }
+}];
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/api', function (req, res) {
+  res.send('You have accessed the API route');
+});
+
+app.get('/api/bars', function (req, res) {
+  if (req.queryParams !== undefined && req.queryParams.name !== undefined) {
+    let filteredBars = bars.filter(function (i) {
+      return i.attributes.name.toLowerCase().indexOf(req.queryParams.name.toLowerCase()) !== -1;
+    });
+    res.send({ data: filteredBars });
+  } else {
+    res.send({ data: bars });
+  }
+});
+
+app.get('/api/bars:id', function(req, res) {
+  res.send({ data: bars.find((bar) => req.params.id == bar.id) });
+})
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000');
+});
